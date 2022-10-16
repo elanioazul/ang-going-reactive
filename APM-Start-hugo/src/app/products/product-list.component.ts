e9923237a383db428cd289648be402cb20fb067c
Hugo Herrador Carrasco <hugo.herrador@geograma.com>
2022-10-17 00:53:39 +0200

    Simple filter based on a harcoded property value

--- APM-Start-hugo/src/app/products/product-list.component.ts
+++ APM-Start-hugo/src/app/products/product-list.component.ts
@@ -1,7 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
-import { catchError, EMPTY, Observable, of } from 'rxjs';
+import { catchError, EMPTY, filter, map, Observable, of } from 'rxjs';
 import { ProductCategory } from '../product-categories/product-category';
+import { ProductCategoryService } from '../product-categories/product-category.service';
 
 import { Product } from './product';
 import { ProductService } from './product.service';
@@ -14,7 +15,7 @@ import { ProductService } from './product.service';
 export class ProductListComponent {
   pageTitle = 'Product List';
   errorMessage = '';
-  categories: ProductCategory[] = [];
+  selectedCategoryId = 1;
 
   products$? = this.productService.productsWithCategory$.pipe(
     catchError(err => {
@@ -23,13 +24,29 @@ export class ProductListComponent {
     })
   );
 
-  constructor(private productService: ProductService) { }
+  categories$ = this.productCategoryService.productCategories$
+    .pipe(
+      catchError(err => {
+        this.errorMessage = err;
+        return EMPTY;
+      })
+    )
+
+  productsSimpleFilter$ = this.productService.productsWithCategory$
+    .pipe(
+      map(products => 
+        products.filter(product => 
+          this.selectedCategoryId ? product.categoryId === this.selectedCategoryId : true
+       ))
+    )
+
+  constructor(private productService: ProductService, private productCategoryService: ProductCategoryService) { }
 
   onAdd(): void {
     console.log('Not yet implemented');
   }
 
   onSelected(categoryId: string): void {
-    console.log('Not yet implemented');
+    this.selectedCategoryId = +categoryId;
   }
 }

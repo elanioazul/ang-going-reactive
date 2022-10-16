8d5e682a2e1f1b53e5eaefb42b88ffae10424574
Hugo Herrador Carrasco <hugo.herrador@geograma.com>
2022-10-16 18:10:22 +0200

    Go super reactive means no ngOnInit and ngOnDestroy, since aync pipe handles subscriptions in template for me

--- APM-Start-hugo/src/app/products/product-list.component.ts
+++ APM-Start-hugo/src/app/products/product-list.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 import { catchError, EMPTY, Observable, of } from 'rxjs';
 import { ProductCategory } from '../product-categories/product-category';
@@ -11,24 +11,19 @@ import { ProductService } from './product.service';
   styleUrls: ['./product-list.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent {
   pageTitle = 'Product List';
   errorMessage = '';
   categories: ProductCategory[] = [];
 
-  products$?: Observable<Product[]>;
+  products$? = this.productService.products$.pipe(
+    catchError(err => {
+      this.errorMessage = err;
+      return EMPTY
+    })
+  );
 
   constructor(private productService: ProductService) { }
-
-  ngOnInit(): void {
-    this.products$ = this.productService.getProducts()
-    .pipe(
-      catchError(err => {
-        this.errorMessage = err;
-        return EMPTY
-      })
-    )
-  }
 
   onAdd(): void {
     console.log('Not yet implemented');

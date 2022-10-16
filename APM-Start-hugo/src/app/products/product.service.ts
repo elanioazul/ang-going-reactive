35194476a2eb4580fb9e2be7a7d298a50142ba1e
Hugo Herrador Carrasco <hugo.herrador@geograma.com>
2022-10-16 23:04:25 +0200

    casting, transforming array elements into a new model class with some properties modified as needed

--- APM-Start-hugo/src/app/products/product.service.ts
+++ APM-Start-hugo/src/app/products/product.service.ts
@@ -15,7 +15,11 @@ export class ProductService {
   products$ = this.http.get<Product[]>(this.productsUrl)
   .pipe(
     map(products => 
-      products.map(product => product.price ? product.price * 1.5 : 0)
+      products.map((product) => ({
+        ...product,
+        price: product.price ? product.price * 1.5 : 0,
+        searchKey: [product.productName]
+      } as Product))
     ),
     tap(data => console.log('Products: ', JSON.stringify(data))),
     catchError(this.handleError)

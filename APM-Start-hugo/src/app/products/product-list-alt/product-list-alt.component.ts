6a3887c9c0bbf3568beab45878160b6104d3d588
Hugo Herrador Carrasco <hugo.herrador@geograma.com>
2022-10-17 20:03:23 +0200

    Bind to an observable => ChangeDetectionStrategy.OnPush

--- APM-Start-hugo/src/app/products/product-list-alt/product-list-alt.component.ts
+++ APM-Start-hugo/src/app/products/product-list-alt/product-list-alt.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
 
 import { catchError, EMPTY, Subscription } from 'rxjs';
 
@@ -7,12 +7,13 @@ import { ProductService } from '../product.service';
 
 @Component({
   selector: 'pm-product-list',
-  templateUrl: './product-list-alt.component.html'
+  templateUrl: './product-list-alt.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductListAltComponent {
   pageTitle = 'Products';
   errorMessage = '';
-  selectedProductId = 0;
+  selectedProductId = this.productService.selectedProduct$;
 
   products$? = this.productService.productsWithCategory$.pipe(
     catchError(err => {
